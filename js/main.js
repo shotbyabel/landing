c2608be5bf3ed172575efccbd6953a2c1ae57424
kimo5532 <shotbyabel@gmail.com>
2015-09-23 19:54:43 -0700

    add on click jQuery function for slides

--- js/main.js
+++ js/main.js
@@ -1,1 +1,12 @@
 
+//jQuery function triggers when doc finish loading
+
+$(document).ready(function() {
+
+    $('section#screenshots a').on('click', function() {
+      //select the modal div id / source attribute -
+      //- add the second refence 'this' is the click fucntion access attribute
+        $('div#modal img').attr('src', $(this).attr('data-image-url') )
+    });
+
+});
